e9862ffb39ace7db42b5acb457a01a591ba4989a
atavirag <tavira-garcia@ijclab.in2p3.fr>
2025-02-25 14:26:08 +0100

    Update fitNContrib to add MC fit

--- NContrib_fits/fitNContrib.cxx
+++ NContrib_fits/fitNContrib.cxx
@@ -20,6 +20,7 @@ void fitNContrib() {
     //gSystem->Load("BifurcatedCB_cxx.so");  // Load the compiled library
 
     TFile *file = TFile::Open("~/MyMacros/Correlations_github/AnalysisResults_newModels_2024af.root", "read");
+    TFile *fileMc = TFile::Open("~/MyMacros/Correlations_github/AnalysisResults_k3_PVContrib.root", "read"); // Using old models + short PV range
     TFile *fout = TFile::Open("fitNContrib.root", "RECREATE");
 
     if (!file) {
@@ -27,29 +28,114 @@ void fitNContrib() {
         return;
     }
 
+    if (!fileMc) {
+        cerr << ">> ERROR MC File not well readout" << endl;
+        return;
+    }
+
     // Retrieve histogram
     TDirectory *dir = dynamic_cast<TDirectory*>(file->Get("hf-correlator-d-meson-pairs"));
-    if (!dir) {
+    TDirectory *dirMc = dynamic_cast<TDirectory*>(fileMc->Get("hf-correlator-d-meson-pairs"));
+    if (!dir || !dirMc) {
         cerr << ">> ERROR: Directory 'hf-correlator-d-meson-pairs' not found" << endl;
         return;
     }
 
+
     TH1F *hNContrib = dynamic_cast<TH1F*>(dir->Get("hPVContrib"));
+    TH1F *hNContribMc = dynamic_cast<TH1F*>(dirMc->Get("hNContribMcGen"));
     if (!hNContrib) {
         cerr << ">> ERROR: Histogram 'hPVContrib' not found" << endl;
         return;
     }
+    if (!hNContribMc) {
+        cerr << ">> ERROR: Histogram 'hNContribMcGen' not found" << endl;
+        return;
+    }
 
-    hNContrib->Rebin(5); // Rebin by a factor of 2 (merges 2 bins into 1)
+    hNContrib->Rebin(5); // Rebin by a factor of 5 (merges 5 bins into 1)
+    hNContribMc->Rebin(5); // Rebin by a factor of 5 (merges 5 bins into 1)
+    hNContrib->GetXaxis()->SetRangeUser(10.0, 180.0);
+    hNContribMc->GetXaxis()->SetRangeUser(10.0, 120.0);
 
-    // Define a BifurcatedCB function
+    // Define the fitting function
     double mean = hNContrib->GetMean();
     double rms = hNContrib->GetRMS();
 
+    double meanMc = hNContribMc->GetMean();
+    double rmsMc = hNContribMc->GetRMS();
+
     // Define the variable corresponding to the x-axis (the PV contributors)
-    RooRealVar pvContrib("pvContrib", "PV Contributors", 0.0, 180.0);
-    RooRealVar mu("mu", "Mean", mean-rms, mean - 2*rms, mean + rms);
-    RooRealVar sigma("sigma", "Sigma", rms, 0.0, 2 * rms);
+    RooRealVar pvContrib("pvContrib", "PV Contributors", 10.0, 180.0);
+    RooRealVar mu("mu", "Mean", mean-rms, mean - 4*rms, mean + rms);
+    RooRealVar sigma("sigma", "Sigma", rms, 0.0, 4 * rms);
+
+    RooRealVar pvContribMc("pvContribMc", "PV MC Contributors", 10.0, 120.0); // not needed when I get the right PV MC range
+    RooRealVar muMc("muMc", "Mean", meanMc-rmsMc, meanMc - 2*rmsMc, meanMc + rmsMc);
+    RooRealVar sigmaMc("sigmaMc", "Sigma", rmsMc, 0.0, 2 * rmsMc);
+
+    RooRealVar tail("tail", "Tail parameter", -0.5, -2, 2);
+    RooRealVar tailMc("tailMc", "Tail parameter", -0.5, -2, 2);
+
+    RooNovosibirsk novo("novo", "Novosibirsk Function", pvContrib, mu, sigma, tail);
+    RooNovosibirsk novoMc("novoMc", "Novosibirsk Function", pvContribMc, muMc, sigmaMc, tailMc);
+
+
+    RooDataHist data("data", "Histogram Data", RooArgList(pvContrib), hNContrib);
+    RooDataHist dataMc("dataMc", "Histogram Data MC", RooArgList(pvContribMc), hNContribMc);
+    // Fit to data
+    RooFitResult* fitResult = novo.fitTo(data, RooFit::Extended(true), RooFit::Save());
+    //RooFitResult* fitResult = bifCB->fitTo(data, RooFit::Extended(true), RooFit::Save());
+    //RooFitResult* fitResult = doubleGauss.fitTo(data, RooFit::Save());
+    fitResult->Print("v");
+
+    RooFitResult* fitResultMc = novoMc.fitTo(dataMc, RooFit::Extended(true), RooFit::Save());
+    fitResultMc->Print("v");
+
+    RooPlot* frame = pvContrib.frame();
+    data.plotOn(frame);
+    novo.plotOn(frame);
+
+    RooPlot* frameMc = pvContribMc.frame();
+    dataMc.plotOn(frameMc);
+    novoMc.plotOn(frameMc);
+
+    // Create canvas and plot
+    TCanvas *c = new TCanvas("c", "NContrib Fit", 800, 600);
+    frame->Draw();
+
+    // Save results
+    fout->cd();
+    frame->Write();
+    c->SaveAs("hNContrib_fit_Novosibirsk.png");
+
+    TCanvas *cMc = new TCanvas("cMc", "NContrib Fit MC", 800, 600);
+    frameMc->Draw();
+
+    // Save results
+    fout->cd();
+    frameMc->Write();
+    cMc->SaveAs("hNContrib_MC_fit_Novosibirsk.png");
+
+    // Compute residuals
+    TCanvas *cRes = new TCanvas("cRes", "NContrib Residuals", 800, 600);
+    RooHist* residuals = frame->residHist();
+    RooPlot* residFrame = pvContrib.frame();
+    residFrame->addPlotable(residuals, "P");
+    residFrame->SetTitle("Fit Residuals for PV Contributors");  // Change the title
+    residFrame->Draw();
+    residFrame->Write();
+    cRes->SaveAs("hNContrib_residuals.png");
+
+    // Cleanup
+    fout->Close();
+    file->Close();
+    delete c;
+    delete cMc;
+    delete cRes;
+}
+
+
     /* RooRealVar alphaL("alphaL", "Left Alpha", 1.5, 0.1, 10.0);
     RooRealVar nL("nL", "Left n", 5.0, 0.1, 30.0);
     RooRealVar alphaR("alphaR", "Right Alpha", 2, 0.1, 15.0);
@@ -70,45 +156,3 @@ void fitNContrib() {
     RooGaussian gauss1("gauss1", "Core Gaussian", pvContrib, mu, sigma1);
     RooGaussian gauss2("gauss2", "Wide Gaussian", pvContrib, mu, sigma2);
     RooAddPdf doubleGauss("doubleGauss", "Double Gaussian", RooArgList(gauss1, gauss2), RooArgList(frac)); */
-
-    RooRealVar tail("tail", "Tail parameter", -0.5, -2, 2);
-    RooNovosibirsk novo("novo", "Novosibirsk Function", pvContrib, mu, sigma, tail);
-
-
-    RooDataHist data("data", "Histogram Data", RooArgList(pvContrib), hNContrib);
-    // Fit to data
-    RooFitResult* fitResult = novo.fitTo(data, RooFit::Extended(true), RooFit::Save());
-    //RooFitResult* fitResult = bifCB->fitTo(data, RooFit::Extended(true), RooFit::Save());
-    //RooFitResult* fitResult = doubleGauss.fitTo(data, RooFit::Save());
-    fitResult->Print("v");
-
-
-    RooPlot* frame = pvContrib.frame();
-    data.plotOn(frame);
-    novo.plotOn(frame);
-
-    // Create canvas and plot
-    TCanvas *c = new TCanvas("c", "NContrib Fit", 800, 600);
-    frame->Draw();
-
-    // Save results
-    fout->cd();
-    frame->Write();
-    c->SaveAs("hNContrib_fit_Novosibirsk.png");
-
-    // Compute residuals
-    TCanvas *cRes = new TCanvas("cRes", "NContrib Residuals", 800, 600);
-    RooHist* residuals = frame->residHist();
-    RooPlot* residFrame = pvContrib.frame();
-    residFrame->addPlotable(residuals, "P");
-    residFrame->SetTitle("Fit Residuals for PV Contributors");  // Change the title
-    residFrame->Draw();
-    residFrame->Write();
-    cRes->SaveAs("hNContrib_residuals.png");
-
-    // Cleanup
-    fout->Close();
-    file->Close();
-    delete c;
-    delete cRes;
-}
